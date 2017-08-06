97a39fa11b5890c781b1249ad02a16d310d18f21
Arash Mousavi <mousavi.arash@gmail.com>
2017-08-07 00:37:23 +0430

    Add simple counter component

---
+++ src/components/counter.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import Tock from 'tocktimer';
+import moment from 'moment';
+
+export default class Counter extends React.Component {
+  constructor(props) {
+    super(props)
+
+    const { initialMinutes } = this.props;
+
+    this.state = {
+      minutesLeft: initialMinutes || '25',
+      secondsLeft: '00'
+    }
+  }
+
+  componentDidMount() {
+    const initialStartTime =
+      `00:${this.state.minutesLeft}:${this.state.secondsLeft}`;
+
+    this._startTimer(initialStartTime);
+  }
+
+  componentWillUnmount() {
+    if (!this.timer) return false;
+
+    this.timer.stop();
+    this.timer = null;
+  }
+
+  _startTimer(initialTime) {
+    const timer = new Tock({
+      countdown: true,
+      interval: 1000,
+      callback: () => {
+        const currentTime = moment.utc(
+          moment.duration(timer.lap()).asMilliseconds()
+        )
+
+        this.setState({
+          minutesLeft: currentTime.format('mm'),
+          secondsLeft: currentTime.format('ss')
+        });
+      }
+    });
+
+    this.timer = timer;
+    timer.start(initialTime);
+  }
+
+  render() {
+    return (
+      <div className='counter'>
+        <span className='counter-minutes'>{ this.state.minutesLeft }</span>
+        <span className='counter-colon'>:</span>
+        <span className='counter-seconds'>{ this.state.secondsLeft }</span>
+      </div>
+    )
+  }
+}
--- src/fire_pomodoro.js
+++ src/fire_pomodoro.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Counter from './components/counter';
+import './styles/style.scss';
 
 class FirePomodoro extends React.Component {
   render() {
     return (
-      <div>
-        <h1>This is a test.</h1>
-      </div>
+      <section className='container'>
+        <Counter />
+      </section>
     );
   }
 }
